fa2ff92d2fce99d1bde8209261b0f609e7b6f88f
Gabriel <gabrielangelgf@hotmail.com>
2019-02-10 21:01:13 -0500

    Primera subida, sólo se ha creado un único recurso (Tabla Orders de Northwind) y las relaciones de dicha clase generan problema, se solucionará en posteriores subidas.

---
+++ ServiciosWeb.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ServiciosWeb.Datos.Modelo;
+
+namespace ServiciosWeb.WebApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        NorthwindConection BD = new NorthwindConection();
+        
+        /// <summary>
+        /// Permite consultar la información de todas las órdenes.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Orders> Get()
+        {
+            var orders = BD.Orders.ToList();
+
+            return orders;
+        }
+    }
+}
