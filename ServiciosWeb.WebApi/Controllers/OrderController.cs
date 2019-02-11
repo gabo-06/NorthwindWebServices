c1071d953df3ce4ef13f80f1661ade00e458da0c
Gabriel <gabrielangelgf@hotmail.com>
2019-02-11 12:45:18 -0500

    Modificación del archivo OrderController.cs en el método Get, se ha usado Linq para obtener las órdenes, obteniendo datos del tipo OrderP

--- ServiciosWeb.WebApi/Controllers/OrderController.cs
+++ ServiciosWeb.WebApi/Controllers/OrderController.cs
@@ -5,20 +5,39 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ServiciosWeb.Datos.Modelo;
+using ServiciosWeb.WebApi.Models;
 
 namespace ServiciosWeb.WebApi.Controllers
 {
     public class OrderController : ApiController
     {
         NorthwindConection BD = new NorthwindConection();
-        
+
         /// <summary>
         /// Permite consultar la información de todas las órdenes.
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<Orders> Get()
+        public IEnumerable<OrderP> Get()
         {
-            var orders = BD.Orders.ToList();
+            // var orders = BD.Orders.Take(2).ToList();
+            var orders = (from o in BD.Orders
+                          select new OrderP
+                          {
+                              orderID = o.OrderID,
+                              customer = new CustomerP { CustomerID = o.Customers.CustomerID, CompanyName = o.Customers.CompanyName },
+                              orderDate = o.OrderDate,
+                              requiredDate = o.RequiredDate,
+                              shippedDate = o.ShippedDate,
+                              shipName = o.ShipName,
+                              shipAddress = o.ShipAddress,
+                              shipCity = o.ShipCity,
+                              shipRegion = o.ShipRegion,
+                              shipPostalCode = o.ShipPostalCode,
+                              shipCountry = o.ShipCountry,
+                              employee = new EmployeeP { EmployeeID = o.Employees.EmployeeID, FirstName = o.Employees.FirstName, LastName = o.Employees.LastName },
+                              shipper = new ShipperP { ShipperID = o.Shippers.ShipperID, CompanyName = o.Shippers.CompanyName },
+                              freight = o.Freight
+                          }).Take(1).ToList();
 
             return orders;
         }
--- ServiciosWeb.WebApi/Global.asax.cs
+++ ServiciosWeb.WebApi/Global.asax.cs
@@ -15,6 +15,14 @@ namespace ServiciosWeb.WebApi
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //Evito las referencias circulares al trabajar con Entity FrameWork         
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            //Elimino que el sistema devuelva en XML, sólo trabajaremos con JSON
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
